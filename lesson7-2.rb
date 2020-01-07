59a616eff611444e0632778fe433cb9b1185cfc3
寺田　圭吾 <terada_keigo@teradakeigonoMacBook-Pro.local>
2020-01-07 17:49:20 +0900

    [finish]2-8] git push -u origin master cd ..

---
+++ lesson7-2.rb
@@ -0,0 +1,24 @@
+puts "計算を始めます"
+puts "何回計算を繰り返しますか？"
+
+input=gets.to_i
+# iは１から始まる。
+i=1
+
+while i<=input do
+puts "#{i} 回目の計算"
+puts "２つの値を入力してください"
+a =gets.to_i
+b =gets.to_i
+puts "a=#{a}"
+puts "b=#{b}"
+puts "計算結果を出力します"
+puts "a+b=#{a+b}"
+puts "a-b=#{a-b}"
+puts "a*b=#{a*b}"
+puts "a/b=#{a/b}"
+# 下記iは１ずつ増加するという意味
+i+=1
+
+end
+puts "計算を終了します"
